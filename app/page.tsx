d3a2b7960470ca29159d275848bb41ea588a6bfe
mynameisnastasya <nastasyabaklykova@gmail.com>
2026-09-06 22:11:14 +0700

    Use routed WINK storefront shell

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import WinkOrderShortcut from "@/components/WinkOrderShortcut";
-import WinkStorefrontV4 from "@/components/WinkStorefrontV4";
+import WinkRootShell from "@/components/WinkRootShell";
 
 export default function Home() {
-  return (
-    <>
-      <WinkStorefrontV4 />
-      <WinkOrderShortcut />
-    </>
-  );
+  return <WinkRootShell />;
 }
