e7559828e33918ec47cce16fa64ee972b31e6f8e
mynameisnastasya <nastasyabaklykova@gmail.com>
2026-09-06 20:45:24 +0700

    Surface safe order tracking

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import WinkBackendBridge from "@/components/WinkBackendBridge";
 import WinkExperience from "@/components/WinkExperience";
+import WinkOrderShortcut from "@/components/WinkOrderShortcut";
 
 export default function Home() {
   return (
     <>
       <WinkExperience />
       <WinkBackendBridge />
+      <WinkOrderShortcut />
     </>
   );
 }
