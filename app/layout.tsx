afe87262044687f3cdf648fa78fb1409df0fd780
mynameisnastasya <nastasyabaklykova@gmail.com>
2026-09-10 14:10:01 +0700

    Load V5 visual audit overrides

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ import "./wink-v3.css";
 import "./wink-v3-font.css";
 import "./wink-v3-audit.css";
 import "./wink-v5.css";
+import "./wink-v5-audit.css";
 
 export const metadata: Metadata = {
   robots: WINK_DEMO ? { index: false, follow: false } : undefined,
