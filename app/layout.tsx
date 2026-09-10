e26be005ed786becce2ad5b69a29eebf1923cfb9
mynameisnastasya <nastasyabaklykova@gmail.com>
2026-09-10 09:00:04 +0700

    V4 audit: load final visual fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ import "./wink-v3.css";
 import "./wink-v3-font.css";
 import "./wink-v3-audit.css";
 import "./wink-v4.css";
+import "./wink-v4-audit.css";
 
 export const metadata: Metadata = {
   robots: WINK_DEMO ? { index: false, follow: false } : undefined,
